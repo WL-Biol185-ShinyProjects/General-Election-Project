ba5da0f0c5d3a5a4a9bc3a7bc75444aa7c1ef7d9
Christian Kim <kimc20@mail.wlu.edu>
2019-10-24 12:48:06 -0400

    Renamed "green" to "other" under "party" column

---
+++ 10-24-19.R
@@ -0,0 +1,22 @@
+# Data Manipulation; Thursday 10-24-2019
+
+#From Matt's Push at 12:34PM:
+library(ggplot2)
+library(dplyr)
+library(leaflet)
+
+
+# Pull in raw data and name i
+
+# Delete unwanted columns & rename dataset
+countyData <- select(countypres_2000_2016, -c(office, version))
+
+
+# Add columns
+countyData[is.na(countyData)] <- "other"
+
+View(countyData)
+
+
+#Making "green" = "other" under "party" column
+countyData$party[countyData$party=="green"] <- "other"
