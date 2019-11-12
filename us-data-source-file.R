770a1f2f09f3f25023658daffc12ceb1820adc02
Matt Dodson <dodsonm20@mail.wlu.edu>
2019-11-12 12:21:03 -0500

    made more edits to state data. including the creation of a nice visual to put on the webpage.

--- us-data-source-file.R
+++ us-data-source-file.R
@@ -19,10 +19,55 @@ stateData$party[stateData$party %notin% c("democrat","republican")] <- "other"
 #changes values that are NA to other 
 stateData[is.na(stateData)] <- "other"
 
-stateData <- select(stateData, c("year", "state", "state_po", "party", "candidatevotes"))
+stateData <- select(stateData, c("year", "state", "state_po", "party", 
+                                 "candidatevotes"))
 
-stateData <- aggregate(candidatevotes~year+state+party+state_po, data = stateData, FUN = sum)
+stateData <- aggregate(candidatevotes~year+state+party+state_po, 
+                       data = stateData, FUN = sum)
 
 stateData <- spread(stateData, party, candidatevotes)
 
-View(stateData)
+stateData$rep_wins <- 0
+stateData$rep_wins[(stateData$republican > stateData$democrat) & 
+                      (stateData$republican > stateData$other)] <- 1
+
+sumStateData <- aggregate(rep_wins~state+state_po, data = stateData, FUN = sum)
+
+sumStateData$color <- "Swing State"
+sumStateData$color[sumStateData$rep_wins >= 6] <- "Republican"
+sumStateData$color[sumStateData$state_po == "AZ"] <- "Swing State"
+sumStateData$color[sumStateData$state_po == "NC"] <- "Swing State"
+sumStateData$color[sumStateData$state_po == "VA"] <- "Swing State"
+sumStateData$color[sumStateData$state_po == "FL"] <- "Swing State"
+sumStateData$color[sumStateData$state_po == "OH"] <- "Swing State"
+sumStateData$color[sumStateData$state_po == "CO"] <- "Democrats"
+sumStateData$color[sumStateData$state_po == "NV"] <- "Democrats"
+sumStateData$color[sumStateData$state_po == "NM"] <- "Democrats"
+sumStateData$color[sumStateData$rep_wins < 5] <- "Democrats"
+sumStateData$color[sumStateData$state_po == "PA"] <- "Swing State"
+sumStateData$color[sumStateData$state_po == "WI"] <- "Swing State"
+sumStateData$color[sumStateData$state_po == "MN"] <- "Swing State"
+
+
+#View(sumStateData)
+
+
+sumStateData %>% 
+  ggplot(aes(x = reorder(state_po, -rep_wins), y = rep_wins, fill = color)) + 
+  geom_bar(stat = "identity")  +
+  scale_fill_manual(values = c("blue", "red", "grey"))
+
+
+probabilityData <- probability_data
+
+probabilityData$winner <- "none"
+probabilityData$winner[probabilityData$electoralVotesNumber < 5] <- "republican"
+probabilityData$winner[probabilityData$electoralVotesNumber > 20] <- "democrat"
+
+sumProbData <- aggregate(electoralVotesNumber~winner, data = probabilityData, FUN = sum)
+
+View(sumProbData)
+
+#repVotes <
+#demVotes
+#freeVotes
